f6b5d9a6906191e8c561ff220a86eb5a6ff01ebf
Dipesh Mishra <dipeshdimi@gmail.com>
2026-05-25 17:04:28 +0530

    analytics

--- src/routers/AllRoutes.tsx
+++ src/routers/AllRoutes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../views/Home";
 import NotFound from "../views/NotFound";
+import { Analytics } from "@vercel/analytics/next"
 
 const AllRoutes = () => {
   return (
     <div>
+      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
